53b281d9a09f864f5317ee318088fa9128b38c22
r31gN_ <the.reignn@gmail.com>
2016-09-28 09:17:36 +0300

    new stuff for starter

---
+++ src/usersListComponent_bk.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+import React from 'react';
+import UserComponent from './userComponent';
+import request from 'superagent';
+
+
+class UsersListComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return null;
+  }
+};
+
+
+export default UsersListComponent;
--- tests/tests.js
+++ tests/tests.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow, mount, render } from 'enzyme';
 import sinon from 'sinon';
+import UserComponent from '../src/userComponent_bk';
+import UsersListComponent from '../src/usersListComponent_bk';
 
 
 describe('Test suite for `TDD in React using Enzyme` workshop', function() {
